b8c5c5e3472a18a5785706c16670bde13d3448bc
fadel <informfadel@gmail.com>
2026-02-01 01:47:41 +0800

    setting up CAN

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,55 @@
-#include <ACAN_ESP32.h>
+#include "esp_twai.h"
+#include "esp_twai_onchip.h"
+#include "esp_log.h"
+#include "string.h"
 
-#define LED_BUILTIN 38
+static const char *TAG = "DEBUG"; // Define a tag for log messages
 
-void setup () {
-  Serial.begin (9600) ;
-  Serial.println ("Hello") ;
-  ACAN_ESP32_Settings settings (125 * 1000) ; // 125 kbit/s
-  settings.mRequestedCANMode = ACAN_ESP32_Settings::LoopBackMode ;
-  const uint32_t errorCode = ACAN_ESP32::can.begin (settings) ;
-  if (0 == errorCode) {
-    Serial.println ("Can ok") ;
-  }else{
-    Serial.print ("Error Can: 0x") ;
-    Serial.println (errorCode, HEX) ;
-  }
+twai_node_handle_t node_hdl = NULL;
+twai_onchip_node_config_t node_config = {
+    .io_cfg = {
+        .tx = 4,       // TWAI TX GPIO pin
+        .rx = 5,       // TWAI RX GPIO pin
+    },
+    .bit_timing = {.bitrate = 200000,},  // 200 kbps bitrate
+    .tx_queue_depth = 5,        // Transmit queue depth set to 5
+};
+
+static bool twai_rx_cb(twai_node_handle_t handle, const twai_rx_done_event_data_t *edata, void *user_ctx)
+{
+    uint8_t recv_buff[8];
+    twai_frame_t rx_frame = {
+        .buffer = recv_buff,
+        .buffer_len = sizeof(recv_buff),
+    };
+    if (ESP_OK == twai_node_receive_from_isr(handle, &rx_frame)) {
+        // receive ok, do something here
+        ESP_LOGI(TAG, "RECEIVED");
+        // Print buffer as hex
+        ESP_LOG_BUFFER_HEX(TAG, rx_frame.buffer, rx_frame.buffer_len);
+        // Or if it's a null-terminated string:
+        // ESP_LOGI(TAG, "Data: %s", (char*)rx_frame.buffer);
+    }
+    return false;
 }
 
-static uint32_t gBlinkLedDate = 0 ;
-static uint32_t gReceivedFrameCount = 0 ;
-static uint32_t gSentFrameCount = 0 ;
-
-
-void loop () {
-  CANMessage frame ;
-  if (gBlinkLedDate < millis ()) {
-    gBlinkLedDate += 500 ;
-    digitalWrite (LED_BUILTIN, !digitalRead (LED_BUILTIN)) ;
-    const bool ok = ACAN_ESP32::can.tryToSend (frame) ;
-    if (ok) {
-      gSentFrameCount += 1 ;
-    }
-  }
-  while (ACAN_ESP32::can.receive (frame)) {
-    gReceivedFrameCount += 1 ;
-  }
+void setup_twai() {
+    // Create a new TWAI controller driver instance
+    ESP_ERROR_CHECK(twai_new_node_onchip(&node_config, &node_hdl));
+    twai_event_callbacks_t user_cbs = {
+    .on_rx_done = twai_rx_cb,
+    };
+    ESP_ERROR_CHECK(twai_node_register_event_callbacks(node_hdl, &user_cbs, NULL));
+    // Start the TWAI controller
+    ESP_ERROR_CHECK(twai_node_enable(node_hdl));
+}
+void send_msg(uint8_t send_buff[8] = {0}) {
+    twai_frame_t tx_msg = {};
+    tx_msg.header.id = 0x1;           // Message ID
+    tx_msg.header.ide = true;         // Use 29-bit extended ID format
+    tx_msg.buffer = send_buff;        // Pointer to data to transmit
+    tx_msg.buffer_len = sizeof(send_buff);  // Length of data to transmit
+    ESP_ERROR_CHECK(twai_node_transmit(node_hdl, &tx_msg, 0));  // Timeout = 0: returns immediately if queue is full
+    // ESP_ERROR_CHECK(twai_node_transmit_wait_all_done(node_hdl, -1));  // Wait for transmission to finish
+    ESP_ERROR_CHECK(twai_event_callbacks_t::on_tx_done(node_hdl, &user_cbs, NULL));
 }
